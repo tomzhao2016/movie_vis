32a3a698fd2ef64fa5cb4ef4da44b88a5a9d0995
QingyangZhao <276676925@qq.com>
2019-03-24 15:27:03 +0800

    add basic structure of circularPlate

---
+++ movie_tree/circularPlate.py
@@ -0,0 +1,29 @@
+class circularPlate(object):
+    """
+    This class creates a compass for the dialog records. Given a record, it returns the coordinates,
+    of the start and end time points.
+    """
+    def __init__(self,canvas_size=(500,500),start_angle=0,radius_scale=10,total_time,person_nums):
+        """
+        initial inputs:
+            canvas_size: (height,width) tuple of integers. assert height==width.
+            start_angle: integer scalar, the angle of start time, default is 0. range(0,360).
+            total_time: integer scalar, movie duration in seconds.
+            radius_scale: float. default 10, the true raidus is person_id*radius_scale.
+            person_id: integer scalar, propotional to radius.
+        """
+        
+    def drawCircular(self):
+        # draw details for the circular
+        pass
+    
+    def getCoordinates(self,start_time,person_id):
+        pass
+        # return x,y
+        
+    
+    
+    
+        
+        
+        
