e953b077311737291f5a6fad11e48fae5c00dd51
QingyangZhao <276676925@qq.com>
2019-04-06 16:30:40 +0800

    null file

--- movie_tree/test_circularPlate.py
+++
@@ -1,8 +0,0 @@
-# from circularPlate import circularPlate
-# cp = circularPlate(canvas_size=(500,500),start_angle=0,total_time='013000',person_nums=10)
-# x,y = cp.getCoordinates('004500',9)
-from circularPlate import sectorPlate
-cp = sectorPlate(canvas_size=(500,500),total_time='013000',person_nums=10)
-x,y = cp.getCoordinates('004500',9)
-print(cp.radius_scale)
-print(x,y)
